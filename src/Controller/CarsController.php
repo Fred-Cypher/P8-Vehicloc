3a71ee3ec2ff94afb0629be83d6b5c74975384d5
Fred Perrier <frederique.perrier@gmail.com>
2025-06-06 16:33:33 +0200

    Add show car and delete car

--- src/Controller/CarsController.php
+++ src/Controller/CarsController.php
@@ -3,19 +3,54 @@
 namespace App\Controller;
 
 use App\Repository\CarRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CarsController extends AbstractController
 {
-    #[Route('/', name:'app_home')]
-    public function index(CarRepository $carRepository): Response
+    public function __construct(private CarRepository $carRepository, private EntityManagerInterface $em)
     {
-        $cars = $carRepository->findAll();
+
+    }
+
+    #[Route('/', name:'app_home')]
+    public function index(): Response
+    {
+        $cars = $this->carRepository->findAll();
 
         return $this->render('home.html.twig', [
             'cars' => $cars,
         ]);
+    }
+
+    #[Route('car/{id}', name: 'app_show_car')]
+    public function showCar(int $id): Response
+    {
+        $car = $this->carRepository->find($id);
+
+        if(!$car){
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('showCar.html.twig', [
+            'car' => $car,
+        ]);
+    }
+
+    #[Route('car/{id}/delete', name: 'app_car_delete', )]
+    public function deleteCar(int $id): Response
+    {
+        $car = $this->carRepository->find($id);
+
+        if (!$car) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $this->em->remove($car);
+        $this->em->flush();
+
+        return $this->redirectToRoute('app_home');
     }
 }
